c1b111574fb21f28c338020a9d0059ff028215f3
Giuseppe Petraroli <94477373+gpetraroli@users.noreply.github.com>
2022-04-04 10:14:21 +0200

    Add files via upload

---
+++ Composer - 2. PSR-4 & Autoload/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'gpetraroli/composer2',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gpetraroli/composer2' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
